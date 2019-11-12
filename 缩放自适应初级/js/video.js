9d4c3a015ddf1894f39be21b1005ff19fd6b75b4
Han-Bin520 <57655484+Han-Bin520@users.noreply.github.com>
2019-11-12 17:47:57 +0800

    suofang

---
+++ 缩放自适应初级/js/video.js
@@ -0,0 +1,37 @@
+let stageWidth,stageHeight,stageScale;
+let video = document.getElementById("video");
+
+resize();
+window.onresize = function(){
+    resize();
+};
+
+function resize()
+{
+    if(stageWidth!=document.documentElement.clientWidth||stageHeight!= document.documentElement.clientHeight)
+    {
+        stageWidth =  document.documentElement.clientWidth;
+        stageHeight = document.documentElement.clientHeight;
+        stageScale = stageWidth/1920;
+
+        video.style.left = 0 + 'px';
+        video.style.top = 0 + 'px';
+        stageScale = stageHeight/1000;
+        video.width = stageScale * 1920;
+        video.height = stageScale * 1000;
+        video.style.left = stageWidth/2 - video.width/2+ 'px';
+        if(stageWidth > video.width)
+        {
+            video.width = 1920;
+            video.height = 1000;
+            video.style.left = 0 + 'px';
+            video.style.top = 0 + 'px';
+            stageScale = stageWidth/1920;
+            video.width = stageScale * 1920;
+            video.height = stageScale * 1000;
+            video.y = 0;
+        }
+
+
+    }
+}
